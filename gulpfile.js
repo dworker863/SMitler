9274bf1138621a4503c9611dbd49ab91bf2cd096
Timur Jalilov <dworker863@gmail.com>
2019-10-22 14:00:04 +0600

    Gulp initialization

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const newer = require("gulp-newer");
+const imageResize = require("gulp-image-resize")
+const imagemin = require("gulp-imagemin");
+const imgCompress = require("imagemin-jpeg-recompress");
+
+function styles() {
+    return gulp.src("app/sass/**/*.sass")
+        .pipe(plumber({
+            errorHandler: notify.onError(function (err) {
+                return {
+                    title: "Styles",
+                    message: err.message + " on line " + err.line,
+                };
+            })
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(concat("styles.min.css"))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return gulp.src([
+        // "app/js/libs.js",
+        "app/js/main.js"
+    ])
+        .pipe(plumber({
+            errorHandler: notify.onError(function (err) {
+                return {
+                    title: "Scripts",
+                    message: err.message + " on line " + err.line,
+                };
+            })
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(concat("scripts.min.js"))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("app/js"))
+        .pipe(browserSync.stream())
+}
+
+function images(done) {
+    [100, 300, 800, 1000, 2000].forEach(function (size) {
+        gulp.src("app/img/src/**/*.{png,jpg,jpeg,webp,raw}")
+        .pipe(newer("app/img/" + size))
+        .pipe(imageResize({width: size}))
+        .pipe(imagemin([
+            imgCompress({
+                loops: 4,
+                min: 70,
+                max: 80,
+                quality: 'high'
+            }),
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest("app/img/" + size))
+        .pipe(browserSync.stream());
+    });    
+    done();
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    gulp.watch("app/**/*.html").on("change", browserSync.reload);
+    gulp.watch("app/sass/**/*.sass", gulp.series(styles));
+    gulp.watch(["app/js/main.js", "app/js/libs.js"], gulp.series(scripts));
+    gulp.watch("app/img/src/**/*.{png,jpg,jpeg,webp,raw}", gulp.series(images))
+}
+
+exports.default = gulp.series(gulp.parallel(styles, scripts, images), watch);
