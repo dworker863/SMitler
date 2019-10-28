b8e34a9c40422953d86a1ea26c854f03753ccfef
Timur Jalilov <dworker863@gmail.com>
2019-10-29 00:47:16 +0600

    Hamburger animation and mmenu sidebar added

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,20 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let menu = new Mmenu("#my-menu", {
+        extensions: ["theme-black", "fx-menu-slide", "pagedim-black", "position-right"],
+        navbar: {
+            title: "<img src=\"img/src/logo-1.svg\">"
+        },
+    });
+
+    let api = menu.API;
+
+    api.bind("open:finish", () => {
+        $(".hamburger").addClass("is-active");
+    });
+
+    api.bind("close:finish", () => {
+        $(".hamburger").removeClass("is-active");
+    })
+});
